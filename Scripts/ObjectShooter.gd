4e2efe34fb93be687d63d192bf28b2368071012f
John James Gutib <jjgutib@gmail.com>
2024-08-24 18:41:55 +0800

    object scenario keyframing

--- Scripts/ObjectShooter.gd
+++ Scripts/ObjectShooter.gd
@@ -2,6 +2,8 @@ extends Node3D
 
 @export var object_to_shoot: PackedScene
 
+var objects_shot = []
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -11,8 +13,14 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
+func cleanup():
+	for object in objects_shot:
+		object.queue_free()
+	objects_shot.clear()
+
 func shoot():
 	var object = object_to_shoot.instantiate()
+	objects_shot.append(object)
 	get_node("../").add_child(object)
 	object.global_position = global_position
 	object.global_rotation = global_rotation
