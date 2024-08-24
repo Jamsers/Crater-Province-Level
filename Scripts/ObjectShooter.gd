18878602696c96e9d58827cfe2ccdbad16ecb32e
John James Gutib <jjgutib@gmail.com>
2024-08-24 17:01:36 +0800

    object shooter

---
+++ Scripts/ObjectShooter.gd
@@ -0,0 +1,21 @@
+extends Node3D
+
+@export var object_to_shoot: PackedScene
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func shoot():
+	var object = object_to_shoot.instantiate()
+	get_node("../").add_child(object)
+	object.global_position = global_position
+	object.global_rotation = global_rotation
+	#object.global_basis.z = global_basis.y
+	#object.global_basis.y = -global_basis.z
+	object.apply_central_impulse(-global_basis.z * 500)
