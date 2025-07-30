ef936c35a57187e89d37f361736ba7a5a20f1585
John James Gutib <jjgutib@gmail.com>
2025-07-31 01:37:01 +0800

    implement bootloader so that the heavy scene doesn't open up by default when opening the project

--- load_heavy_shit.gd
+++ load_heavy_shit.gd
@@ -4,6 +4,5 @@ extends Node
 @export var should_load: bool = true
 
 func _ready():
-	get_node("../../WorldEnvironment").environment.sdfgi_enabled = true
 	if should_load:
 		add_child(heavy_shit.instantiate())
